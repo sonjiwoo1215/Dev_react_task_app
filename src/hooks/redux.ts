3efea77d64f80312686697b9305893476438c8dd
j1w0os <102014705+sonjiwoo1215@users.noreply.github.com>
2025-04-10 19:14:42 +0900

    0410

---
+++ src/hooks/redux.ts
@@ -0,0 +1,8 @@
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux"
+import { AppDispatch, RootState} from "../store";
+
+export const useTypedSelector: TypedUseSelectorHook<RootState> = useSelector
+export const useTypedDispatch = ()=> useDispatch<AppDispatch>();
+
+// const logger = useTypedSelector((state) => state.logger);
+
